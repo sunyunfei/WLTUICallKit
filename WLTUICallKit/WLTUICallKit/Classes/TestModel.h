d3cb099fc5e4a032913ce9132fb5cdbe0c806eb6
sunyunfei <>
2023-05-10 16:01:32 +0800

    test

---
+++ WLTUICallKit/WLTUICallKit/Classes/TestModel.h
@@ -0,0 +1,16 @@
+//
+//  TestModel.h
+//  podTest
+//
+//  Created by sunyunfei on 2023/5/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WLTUICallKit/WLTUICallKit/Classes/TestModel.m
@@ -0,0 +1,12 @@
+//
+//  TestModel.m
+//  podTest
+//
+//  Created by sunyunfei on 2023/5/10.
+//
+
+#import "TestModel.h"
+
+@implementation TestModel
+
+@end
